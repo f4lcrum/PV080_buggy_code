eb74c71584b76dd8ebc69b42bf750915ba931294
Kristian Oravec <xoravec2@fi.muni.cz>
2023-03-28 08:47:44 +0200

    Fix: some errors mentioned by Codacy

--- buggy_python_code.py
+++ buggy_python_code.py
@@ -1,7 +1,8 @@
-import sys 
+import sys
 import os
-import yaml
 import flask
+import yaml
+
 
 app = flask.Flask(__name__)
 
@@ -12,14 +13,30 @@ def index():
     url = flask.request.args.get("url")
     return fetch_website(version, url)
 
-        
+
 CONFIG = {"API_KEY": "771df488714111d39138eb60df756e6b"}
 class Person(object):
+    '''
+    Object of Person
+    Parameters:
+        name: a name of person
+
+    Returns:
+        Nothing
+    '''
     def __init__(self, name):
         self.name = name
 
 
 def print_nametag(format_string, person):
+    '''
+    Prints a name of person
+    Parameters:
+        format_string: formats persona name
+        person: instance of object of person
+    return:
+        Nothing
+    '''
     print(format_string.format(person=person))
 
 
@@ -27,8 +44,8 @@ def fetch_website(urllib_version, url):
     # Import the requested version (2 or 3) of urllib
     exec(f"import urllib{urllib_version} as urllib", globals())
     # Fetch and print the requested URL
- 
-    try: 
+
+    try:
         http = urllib.PoolManager()
         r = http.request('GET', url)
     except:
@@ -39,7 +56,7 @@ def load_yaml(filename):
     stream = open(filename)
     deserialized_data = yaml.load(stream, Loader=yaml.Loader) #deserializing data
     return deserialized_data
-    
+
 def authenticate(password):
     # Assert that the password is correct
     assert password == "Iloveyou", "Invalid password!"
@@ -47,13 +64,16 @@ def authenticate(password):
 
 if __name__ == '__main__':
     print("Vulnerabilities:")
-    print("1. Format string vulnerability: use string={person.__init__.__globals__[CONFIG][API_KEY]}")
+    print("""
+           1. Format string vulnerability:
+           use string={person.__init__.__globals__[CONFIG][API_KEY]}
+           """)
     print("2. Code injection vulnerability: use string=;print('Own code executed') #")
     print("3. Yaml deserialization vulnerability: use string=file.yaml")
     print("4. Use of assert statements vulnerability: run program with -O argument")
     choice  = input("Select vulnerability: ")
-    if choice == "1": 
-        new_person = Person("Vickie")  
+    if choice == "1":
+        new_person = Person("Vickie")
         print_nametag(input("Please format your nametag: "), new_person)
     elif choice == "2":
         urlib_version = input("Choose version of urllib: ")
@@ -64,4 +84,3 @@ if __name__ == '__main__':
     elif choice == "4":
         password = input("Enter master password: ")
         authenticate(password)
-
